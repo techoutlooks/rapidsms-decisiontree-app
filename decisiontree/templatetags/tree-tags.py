95e9dc8775d177b2f5e9ccf94228d419f42c2710
Dan Poirier <dpoirier@caktusgroup.com>
2011-05-16 11:12:41 -0400

    Souktel-alert version of decisiontree

--- decisiontree/templatetags/tree-tags.py
+++ decisiontree/templatetags/tree-tags.py
@@ -1,5 +1,48 @@
-from django.template import Library
-register = Library()
+from django import template
+from django.template.defaultfilters import stringfilter
+
+
+register = template.Library()
+
+
+@register.filter
+def mean(values):
+    try:
+        integers = [int(v) for v in values]
+    except (TypeError, ValueError):
+        return 'n/a'
+    return sum(integers)/len(integers)*1.0
+
+
+@register.filter
+def median(values):
+    try:
+        values = [int(v) for v in values]
+    except (TypeError, ValueError):
+        pass
+    values = sorted(values)
+    size = len(values)
+    try:
+        if size % 2 == 1:
+            return values[(size - 1) / 2]
+        else:
+            values = [int(v) for v in values]
+            return (values[size/2 - 1] + values[size/2]) / 2
+    except (TypeError, ValueError):
+        return 'n/a'
+
+
+@register.filter
+def mode(values):
+    copy = sorted(values)
+    counts = {}
+    for x in set(copy):
+        count = copy.count(x)
+        if count not in counts:
+            counts[count] = []
+        counts[count].append(x)
+    return counts[max(counts.keys())]
+
 
 @register.inclusion_tag("tree/partials/tree.html")
 def render_tree(tree):
