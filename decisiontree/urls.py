cc5e9f9dbc7c5dceb23ac22ecb81225bca048ade
Rebecca Lovewell <rebecca@caktusgroup.com>
2014-05-23 11:20:06 -0400

    Update URLs import

--- decisiontree/urls.py
+++ decisiontree/urls.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
-from django.conf.urls.defaults import patterns, url
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, url
 
 from decisiontree import views
 
