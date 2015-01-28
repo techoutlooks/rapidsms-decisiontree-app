ab74404cbf6c614a677fd14f53a4b544d17a54da
Rebecca <rebecca@caktusgroup.com>
2015-01-28 13:29:57 -0500

    Fix incorrect migration reference

--- decisiontree/migrations/0001_initial.py
+++ decisiontree/migrations/0001_initial.py
@@ -8,7 +8,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('rapidsms', '0002_delete_backendmessage'),
+        ('rapidsms', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
