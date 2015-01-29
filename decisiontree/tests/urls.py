d9f26a1d2831546517aec163a447dcf83cc80701
Rebecca <rebecca@caktusgroup.com>
2015-01-29 10:22:02 -0500

    Remove reference to the scheduler, which was removed.

--- decisiontree/tests/urls.py
+++ decisiontree/tests/urls.py
@@ -8,6 +8,5 @@ admin.autodiscover()
 urlpatterns = [
     (r'^admin/', include(admin.site.urls)),
     (r'^account/', include('rapidsms.urls.login_logout')),
-    (r'^scheduler/', include('rapidsms.contrib.scheduler.urls')),
     (r'^tree/', include('decisiontree.urls')),
 ]
