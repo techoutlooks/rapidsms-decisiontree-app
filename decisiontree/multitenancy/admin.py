b43824a63039e7a2693b8bf4408efe527f95d8d3
Rebecca <rebecca@caktusgroup.com>
2015-02-04 16:59:32 -0500

    Dry out multitenancy admin code.

---
+++ decisiontree/multitenancy/admin.py
@@ -0,0 +1,108 @@
+from copy import copy
+
+from django.contrib import admin
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.encoding import force_text
+
+from decisiontree import models as tree_models
+from decisiontree import admin as tree_admin
+
+from . import models as link_models
+from . import utils
+from .forms import RequiredInlineFormSet
+
+
+class ByTenantListFilter(admin.SimpleListFilter):
+    """Allow users to filter objects by the tenants they manage."""
+    title = 'tenant'
+    parameter_name = 'tenant'
+
+    def lookups(self, request, model_admin):
+        tenants = utils.get_tenants_for_user(request.user)
+        return [(tenant.pk, force_text(tenant)) for tenant in tenants]
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            queryset = queryset.filter(tenantlink__tenant=self.value())
+        return queryset
+
+
+class TenantLinkInlineMixin(object):
+    """Require exactly one tenant link."""
+    formset = RequiredInlineFormSet
+
+    def has_delete_permission(self, request, obj=None):
+        """Tenant links cannot be deleted, though they can be modified."""
+        return False
+
+
+class MultitenancyAdminMixin(object):
+    """Add information about the tenant to an existing model admin."""
+
+    @property
+    def tenant_link_model(self):
+        raise ImproperlyConfigured("Implementing class must specify the "
+                                   "tenant link model.")
+
+    def get_tenant_link_inline(self, request, obj=None):
+        """Return the inline class for the model's tenant link class."""
+        inline_name = '{}Inline'.format(self.tenant_link_model.__name__)
+        return type(inline_name, (TenantLinkInlineMixin, admin.TabularInline), {
+            'model': self.tenant_link_model,
+        })
+
+    def get_inline_instances(self, request, obj=None):
+        """Add an inline for the object's tenant link."""
+        link_inline = self.get_tenant_link_inline(request, obj)
+        _original_inlines = copy(self.inlines)
+        self.inlines = [link_inline] + list(self.inlines)
+        instances = super(MultitenancyAdminMixin, self).get_inline_instances(request, obj)
+        self.inlines = _original_inlines
+        return instances
+
+    def get_list_display(self, request):
+        """Add the tenant as the last column on the list page."""
+        list_display = super(MultitenancyAdminMixin, self).get_list_display(request)
+        return list(list_display) + ['tenant']
+
+    def get_list_filter(self, request):
+        """Allow filtering objects by tenant."""
+        list_filter = super(MultitenancyAdminMixin, self).get_list_filter(request)
+        return [ByTenantListFilter] + list(list_filter)
+
+    def get_queryset(self, request):
+        """Only show objects for tenants the user manages."""
+        tenants = utils.get_tenants_for_user(request.user)
+        qs = super(MultitenancyAdminMixin, self).get_queryset(request)
+        return qs.filter(tenantlink__tenant__in=tenants)
+
+    def tenant(self, obj):
+        return obj.tenantlink.tenant
+
+
+if utils.multitenancy_enabled():
+    # Unregister tree models, and re-register them with tenant-enabled admins.
+    # FIXME: This assumes that the tree models have already been registered
+    # (i.e., that `decisiontree` comes before `decisiontree.multitenancy` in
+    # when apps are loaded)
+
+    tree_models = [
+        (tree_models.Answer, tree_admin.AnswerAdmin),
+        (tree_models.Entry, tree_admin.EntryAdmin),
+        (tree_models.Question, tree_admin.QuestionAdmin),
+        (tree_models.Session, tree_admin.SessionAdmin),
+        (tree_models.TagNotification, tree_admin.TagNotificationAdmin),
+        (tree_models.Tag, tree_admin.TagAdmin),
+        (tree_models.Transition, tree_admin.TransitionAdmin),
+        (tree_models.Tree, tree_admin.TreeAdmin),
+        (tree_models.TreeState, tree_admin.StateAdmin),
+    ]
+
+    for model, model_admin in tree_models:
+        link_model = utils.get_tenant_link_from_model(model)
+        admin.site.unregister(model)
+        admin_name = 'Multitenancy{}'.format(model_admin.__name__)
+        ModelMultitenancyAdmin = type(admin_name, (MultitenancyAdminMixin, model_admin), {
+            'tenant_link_model': link_model,
+        })
+        admin.site.register(model, ModelMultitenancyAdmin)
--- decisiontree/multitenancy/utils.py
+++ decisiontree/multitenancy/utils.py
@@ -1,5 +1,23 @@
 from django.conf import settings
+from django.db.models import Q
 
 
 def multitenancy_enabled():
     return "decisiontree.multitenancy" in settings.INSTALLED_APPS
+
+
+def get_tenants_for_user(user):
+    """Return all tenants that the user can manage."""
+    from multitenancy.models import Tenant
+    tenants = Tenant.objects.all()
+    if not user.is_superuser:
+        user_is_manager = Q(tenantrole__user=user) | Q(group__tenantrole__user=user)
+        tenants = tenants.filter(user_is_manager)
+    return tenants
+
+
+def get_tenant_link_from_model(model):
+    """Get the tenant link model associated with the model class."""
+    if not hasattr(model, 'tenantlink'):
+        raise TypeError("")  # TODO
+    return model.tenantlink.related.model
