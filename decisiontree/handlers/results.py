045ae4b152e2892fba158df51572c56e34800ad2
Rebecca <rebecca@caktusgroup.com>
2015-02-11 16:09:27 -0500

    Limit survey results to correct tenant

--- decisiontree/handlers/results.py
+++ decisiontree/handlers/results.py
@@ -4,20 +4,27 @@ from decisiontree.models import Tree
 
 
 class ResultsHandler(KeywordHandler):
-
     keyword = "results"
 
     def help(self):
         self.respond("Please enter a survey keyword")
 
+    def get_survey(self, trigger):
+        """Limit survey pool to those for the sender's tenant."""
+        from decisiontree.multitenancy.utils import multitenancy_enabled
+        queryset = Tree.objects.filter(trigger=trigger)
+        if multitenancy_enabled():
+            tenant = self.msg.connection.backend.tenantlink.tenant
+            queryset = queryset.filter(tenantlink__tenant=tenant)
+        return queryset[0] if queryset else None
+
     def handle(self, text):
-        try:
-            tree = Tree.objects.get(trigger=text)
-        except Tree.DoesNotExist:
+        """Send the summary of the survey, if one exists."""
+        survey = self.get_survey(trigger=text)
+        if not survey:
             self.respond('Survey "{0}" does not exist'.format(text))
-            return True
-        if tree.summary:
-            self.respond(tree.summary)
+        elif survey.summary:
+            self.respond(survey.summary)
         else:
             self.respond('No summary for "{0}" survey'.format(text))
         return True
