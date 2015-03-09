e383dbcab054359cb47e7538425e540911243de3
Vinod Kurup <vinod@kurup.com>
2015-03-09 16:11:09 -0400

    Add maxlength plugin to textareas

---
+++ decisiontree/static/tree/javascripts/decisiontree.js
@@ -0,0 +1,27 @@
+$(function () {
+  function addNotification(type, message) {
+    var cls = type === null ? "alert" : "alert alert-" + type;
+    var dismiss = $("<button>").attr("data-dismiss", "alert").addClass("close").html("&times;");
+    var notif = $("<div>").addClass(cls).html(message).prepend(dismiss);
+    $("#messages").append(notif);
+  }
+  function clearNotifications() {
+    $("#messages").children().alert("close");
+  }
+  $('.counterField textarea').maxlength(
+    {max: 160,
+     truncate: false,
+     onFull: function(overflowing){
+       if (!overflowing) {
+         clearNotifications();
+         // enable the submit button
+         $(':button').removeAttr('disabled');
+       } else {
+         clearNotifications();
+         addNotification("error", "Message too long.");
+         // disable the submit button
+         $(':button').attr('disabled', 'disabled');
+       }
+     }
+    });
+});
